f65a69295144a1071092bbcc0cdb5ed8faf8e02d
xyx0826 <xyx0826@hotmail.com>
2020-04-26 18:42:52 -0700

    Add external realloc support

--- revorb.c
+++ revorb.c
@@ -19,10 +19,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/*# INCLUDE=.\include #*/
-/*# LIB=.\lib         #*/
-/*# CFLAGS=/D_UNICODE #*/
-/*# LFLAGS=/NODEFAULTLIB:MSVCRT /LTCG /OPT:REF /MANIFEST:NO #*/
+ /*# INCLUDE=.\include #*/
+ /*# LIB=.\lib         #*/
+ /*# CFLAGS=/D_UNICODE #*/
+ /*# LFLAGS=/NODEFAULTLIB:MSVCRT /LTCG /OPT:REF /MANIFEST:NO #*/
 
 #include "stdafx.h"
 
@@ -41,252 +41,262 @@
 
 #include "revorb.h"
 
+typedef void* (__stdcall* ReAllocHGlobal)(void*, size_t);
+
+ReAllocHGlobal netrealloc;
+
 size_t revorb_fread(void* buffer, size_t size, size_t n, REVORB_FILE* fp) {
-  if (fp->size == -1) {
-    return -1;
-  }
-  size_t sz = size * n;
-  intptr_t offset = (intptr_t)(fp->cursor) - (intptr_t)(fp->start);
-  if (offset + sz > fp->size) {
-    sz = fp->size - offset;
-  }
-  memcpy(buffer, fp->cursor, sz);
-  (intptr_t)fp->cursor += sz;
-  return sz;
+	if (fp->size == -1) {
+		return -1;
+	}
+	size_t sz = size * n;
+	intptr_t offset = (intptr_t)(fp->cursor) - (intptr_t)(fp->start);
+	if (offset + sz > fp->size) {
+		sz = fp->size - offset;
+	}
+	memcpy(buffer, fp->cursor, sz);
+	(intptr_t)fp->cursor += sz;
+	return sz;
 }
 
 size_t revorb_fwrite(void* buffer, size_t size, size_t n, REVORB_FILE* fp) {
-  if (fp->size == -1) {
-    return -1;
-  }
-  size_t sz = size * n;
-  intptr_t offset = (intptr_t)(fp->cursor) - (intptr_t)(fp->start);
-  if (fp->size - offset < sz) {
-      intptr_t delta = sz - (fp->size - offset);
-    fp->size += delta + 1;
-    fp->start = (void*)realloc(fp->start, fp->size);
-    fp->cursor = (void*)((intptr_t)fp->start + offset);
-  }
-  memcpy(fp->cursor, buffer, sz);
-  (intptr_t)fp->cursor += sz;
-  return sz;
+	if (fp->size == -1) {
+		return -1;
+	}
+	size_t sz = size * n;
+	intptr_t offset = (intptr_t)(fp->cursor) - (intptr_t)(fp->start);
+	if (fp->size - offset < sz) {
+		intptr_t delta = sz - (fp->size - offset);
+		fp->size += delta + 1;
+		if (netrealloc) {
+			fp->start = netrealloc(fp->start, fp->size);
+		} else {
+			fp->start = (void*)realloc(fp->start, fp->size);
+		}
+		fp->cursor = (void*)((intptr_t)fp->start + offset);
+	}
+	memcpy(fp->cursor, buffer, sz);
+	(intptr_t)fp->cursor += sz;
+	return sz;
 }
+
 void revorb_fclose(REVORB_FILE* fp) {
-  free(fp->start);
-  fp->start = NULL;
-  fp->cursor = NULL;
-  fp->size = -1;
+	free(fp->start);
+	fp->start = NULL;
+	fp->cursor = NULL;
+	fp->size = -1;
 }
 
 REVORB_RESULT copy_headers(REVORB_FILE* fi,
-                           ogg_sync_state* si,
-                           ogg_stream_state* is,
-                           REVORB_FILE* fo,
-                           ogg_sync_state* so,
-                           ogg_stream_state* os,
-                           vorbis_info* vi) {
-  char* buffer = ogg_sync_buffer(si, 4096);
-  size_t numread = revorb_fread(buffer, 1, 4096, fi);
-  ogg_sync_wrote(si, numread);
+	ogg_sync_state* si,
+	ogg_stream_state* is,
+	REVORB_FILE* fo,
+	ogg_sync_state* so,
+	ogg_stream_state* os,
+	vorbis_info* vi) {
+	char* buffer = ogg_sync_buffer(si, 4096);
+	size_t numread = revorb_fread(buffer, 1, 4096, fi);
+	ogg_sync_wrote(si, numread);
 
-  ogg_page page;
-  if (ogg_sync_pageout(si, &page) != 1) {
-    return REVORB_ERR_NOT_OGG;
-  }
+	ogg_page page;
+	if (ogg_sync_pageout(si, &page) != 1) {
+		return REVORB_ERR_NOT_OGG;
+	}
 
-  ogg_stream_init(is, ogg_page_serialno(&page));
-  ogg_stream_init(os, ogg_page_serialno(&page));
+	ogg_stream_init(is, ogg_page_serialno(&page));
+	ogg_stream_init(os, ogg_page_serialno(&page));
 
-  if (ogg_stream_pagein(is, &page) < 0) {
-    ogg_stream_clear(is);
-    ogg_stream_clear(os);
-    return REVORB_ERR_FIRST_PAGE;
-  }
+	if (ogg_stream_pagein(is, &page) < 0) {
+		ogg_stream_clear(is);
+		ogg_stream_clear(os);
+		return REVORB_ERR_FIRST_PAGE;
+	}
 
-  ogg_packet packet;
-  if (ogg_stream_packetout(is, &packet) != 1) {
-    ogg_stream_clear(is);
-    ogg_stream_clear(os);
-    return REVORB_ERR_FIRST_PACKET;
-  }
+	ogg_packet packet;
+	if (ogg_stream_packetout(is, &packet) != 1) {
+		ogg_stream_clear(is);
+		ogg_stream_clear(os);
+		return REVORB_ERR_FIRST_PACKET;
+	}
 
-  vorbis_comment vc;
-  vorbis_comment_init(&vc);
-  if (vorbis_synthesis_headerin(vi, &vc, &packet) < 0) {
-    vorbis_comment_clear(&vc);
-    ogg_stream_clear(is);
-    ogg_stream_clear(os);
-    return REVORB_ERR_HEADER;
-  }
+	vorbis_comment vc;
+	vorbis_comment_init(&vc);
+	if (vorbis_synthesis_headerin(vi, &vc, &packet) < 0) {
+		vorbis_comment_clear(&vc);
+		ogg_stream_clear(is);
+		ogg_stream_clear(os);
+		return REVORB_ERR_HEADER;
+	}
 
-  ogg_stream_packetin(os, &packet);
+	ogg_stream_packetin(os, &packet);
 
-  int i = 0;
-  while (i < 2) {
-    int res = ogg_sync_pageout(si, &page);
+	int i = 0;
+	while (i < 2) {
+		int res = ogg_sync_pageout(si, &page);
 
-    if (res == 0) {
-      buffer = ogg_sync_buffer(si, 4096);
-      numread = revorb_fread(buffer, 1, 4096, fi);
-      if (numread == 0 && i < 2) {
-        ogg_stream_clear(is);
-        ogg_stream_clear(os);
-        return REVORB_ERR_TRUNCATED;
-      }
-      ogg_sync_wrote(si, 4096);
-      continue;
-    }
+		if (res == 0) {
+			buffer = ogg_sync_buffer(si, 4096);
+			numread = revorb_fread(buffer, 1, 4096, fi);
+			if (numread == 0 && i < 2) {
+				ogg_stream_clear(is);
+				ogg_stream_clear(os);
+				return REVORB_ERR_TRUNCATED;
+			}
+			ogg_sync_wrote(si, 4096);
+			continue;
+		}
 
-    if (res == 1) {
-      ogg_stream_pagein(is, &page);
-      while (i < 2) {
-        res = ogg_stream_packetout(is, &packet);
-        if (res == 0)
-          break;
-        if (res < 0) {
-          vorbis_comment_clear(&vc);
-          ogg_stream_clear(is);
-          ogg_stream_clear(os);
-          return REVORB_ERR_SECONDARY_HEADER;
-        }
-        vorbis_synthesis_headerin(vi, &vc, &packet);
-        ogg_stream_packetin(os, &packet);
-        i++;
-      }
-    }
-  }
+		if (res == 1) {
+			ogg_stream_pagein(is, &page);
+			while (i < 2) {
+				res = ogg_stream_packetout(is, &packet);
+				if (res == 0)
+					break;
+				if (res < 0) {
+					vorbis_comment_clear(&vc);
+					ogg_stream_clear(is);
+					ogg_stream_clear(os);
+					return REVORB_ERR_SECONDARY_HEADER;
+				}
+				vorbis_synthesis_headerin(vi, &vc, &packet);
+				ogg_stream_packetin(os, &packet);
+				i++;
+			}
+		}
+	}
 
-  vorbis_comment_clear(&vc);
+	vorbis_comment_clear(&vc);
 
-  while (ogg_stream_flush(os, &page)) {
-    if (revorb_fwrite(page.header, 1, page.header_len, fo) != page.header_len ||
-        revorb_fwrite(page.body, 1, page.body_len, fo) != page.body_len) {
-      ogg_stream_clear(is);
-      ogg_stream_clear(os);
-      return REVORB_ERR_HEADER_WRITE;
-    }
-  }
+	while (ogg_stream_flush(os, &page)) {
+		if (revorb_fwrite(page.header, 1, page.header_len, fo) != page.header_len ||
+			revorb_fwrite(page.body, 1, page.body_len, fo) != page.body_len) {
+			ogg_stream_clear(is);
+			ogg_stream_clear(os);
+			return REVORB_ERR_HEADER_WRITE;
+		}
+	}
 
-  return REVORB_ERR_SUCCESS;
+	return REVORB_ERR_SUCCESS;
 }
 
-REVORBAPI REVORB_RESULT revorb(REVORB_FILE* fi, REVORB_FILE* fo) {
-  int g_failed;
+REVORBAPI REVORB_RESULT revorb(REVORB_FILE* fi, REVORB_FILE* fo, ReAllocHGlobal reAlloc) {
+	int g_failed;
+	netrealloc = reAlloc;
 
-  ogg_sync_state sync_in, sync_out;
-  ogg_sync_init(&sync_in);
-  ogg_sync_init(&sync_out);
+	ogg_sync_state sync_in, sync_out;
+	ogg_sync_init(&sync_in);
+	ogg_sync_init(&sync_out);
 
-  ogg_stream_state stream_in, stream_out;
-  vorbis_info vi;
-  vorbis_info_init(&vi);
+	ogg_stream_state stream_in, stream_out;
+	vorbis_info vi;
+	vorbis_info_init(&vi);
 
-  ogg_packet packet;
-  ogg_page page;
+	ogg_packet packet;
+	ogg_page page;
 
-  REVORB_RESULT code =
-      copy_headers(fi, &sync_in, &stream_in, fo, &sync_out, &stream_out, &vi);
+	REVORB_RESULT code =
+		copy_headers(fi, &sync_in, &stream_in, fo, &sync_out, &stream_out, &vi);
 
-  if (code == REVORB_ERR_SUCCESS) {
-    ogg_int64_t granpos = 0, packetnum = 0;
-    int lastbs = 0;
+	if (code == REVORB_ERR_SUCCESS) {
+		ogg_int64_t granpos = 0, packetnum = 0;
+		int lastbs = 0;
 
-    while (1) {
-      int eos = 0;
-      while (!eos) {
-        int res = ogg_sync_pageout(&sync_in, &page);
-        if (res == 0) {
-          char* buffer = ogg_sync_buffer(&sync_in, 4096);
-          int numread = revorb_fread(buffer, 1, 4096, fi);
-          if (numread > 0)
-            ogg_sync_wrote(&sync_in, numread);
-          else
-            eos = 2;
-          continue;
-        }
+		while (1) {
+			int eos = 0;
+			while (!eos) {
+				int res = ogg_sync_pageout(&sync_in, &page);
+				if (res == 0) {
+					char* buffer = ogg_sync_buffer(&sync_in, 4096);
+					int numread = revorb_fread(buffer, 1, 4096, fi);
+					if (numread > 0)
+						ogg_sync_wrote(&sync_in, numread);
+					else
+						eos = 2;
+					continue;
+				}
 
-        if (res < 0) {
-          code = REVORB_ERR_CORRUPT;
-          g_failed = 1;
-        } else {
-          if (ogg_page_eos(&page))
-            eos = 1;
-          ogg_stream_pagein(&stream_in, &page);
+				if (res < 0) {
+					code = REVORB_ERR_CORRUPT;
+					g_failed = 1;
+				} else {
+					if (ogg_page_eos(&page))
+						eos = 1;
+					ogg_stream_pagein(&stream_in, &page);
 
-          while (1) {
-            res = ogg_stream_packetout(&stream_in, &packet);
-            if (res == 0)
-              break;
-            if (res < 0) {
-              code = REVORB_ERR_BITSTREAM_CORRUPT;
-              g_failed = 1;
-              continue;
-            }
+					while (1) {
+						res = ogg_stream_packetout(&stream_in, &packet);
+						if (res == 0)
+							break;
+						if (res < 0) {
+							code = REVORB_ERR_BITSTREAM_CORRUPT;
+							g_failed = 1;
+							continue;
+						}
 
-            /*
-            if (packet.granulepos >= 0) {
-              granpos = packet.granulepos + logstream_startgran;
-              packet.granulepos = granpos;
-            }
-            */
-            int bs = vorbis_packet_blocksize(&vi, &packet);
-            if (lastbs)
-              granpos += (lastbs + bs) / 4;
-            lastbs = bs;
+						/*
+						if (packet.granulepos >= 0) {
+						  granpos = packet.granulepos + logstream_startgran;
+						  packet.granulepos = granpos;
+						}
+						*/
+						int bs = vorbis_packet_blocksize(&vi, &packet);
+						if (lastbs)
+							granpos += (lastbs + bs) / 4;
+						lastbs = bs;
 
-            packet.granulepos = granpos;
-            packet.packetno = packetnum++;
-            if (!packet.e_o_s) {
-              ogg_stream_packetin(&stream_out, &packet);
+						packet.granulepos = granpos;
+						packet.packetno = packetnum++;
+						if (!packet.e_o_s) {
+							ogg_stream_packetin(&stream_out, &packet);
 
-              ogg_page opage;
-              while (ogg_stream_pageout(&stream_out, &opage)) {
-                if (revorb_fwrite(opage.header, 1, opage.header_len, fo) !=
-                        opage.header_len ||
-                    revorb_fwrite(opage.body, 1, opage.body_len, fo) !=
-                        opage.body_len) {
-                  code = REVORB_ERR_WRITE_FAIL;
-                  eos = 2;
-                  g_failed = 1;
-                  break;
-                }
-              }
-            }
-          }
-        }
-      }
+							ogg_page opage;
+							while (ogg_stream_pageout(&stream_out, &opage)) {
+								if (revorb_fwrite(opage.header, 1, opage.header_len, fo) !=
+									opage.header_len ||
+									revorb_fwrite(opage.body, 1, opage.body_len, fo) !=
+									opage.body_len) {
+									code = REVORB_ERR_WRITE_FAIL;
+									eos = 2;
+									g_failed = 1;
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
 
-      if (eos == 2)
-        break;
+			if (eos == 2)
+				break;
 
-      { 
-        packet.e_o_s = 1;
-        ogg_stream_packetin(&stream_out, &packet);
-        ogg_page opage;
-        while (ogg_stream_flush(&stream_out, &opage)) {
-          if (revorb_fwrite(opage.header, 1, opage.header_len, fo) !=
-                  opage.header_len ||
-              revorb_fwrite(opage.body, 1, opage.body_len, fo) !=
-                  opage.body_len) {
-            code = REVORB_ERR_WRITE_FAIL2;
-            g_failed = 1;
-            break;
-          }
-        }
-        ogg_stream_clear(&stream_in);
-        break;
-      }
-    }
+			{
+				packet.e_o_s = 1;
+				ogg_stream_packetin(&stream_out, &packet);
+				ogg_page opage;
+				while (ogg_stream_flush(&stream_out, &opage)) {
+					if (revorb_fwrite(opage.header, 1, opage.header_len, fo) !=
+						opage.header_len ||
+						revorb_fwrite(opage.body, 1, opage.body_len, fo) !=
+						opage.body_len) {
+						code = REVORB_ERR_WRITE_FAIL2;
+						g_failed = 1;
+						break;
+					}
+				}
+				ogg_stream_clear(&stream_in);
+				break;
+			}
+		}
 
-    ogg_stream_clear(&stream_out);
-  } else {
-    g_failed = 1;
-  }
+		ogg_stream_clear(&stream_out);
+	} else {
+		g_failed = 1;
+	}
 
-  vorbis_info_clear(&vi);
+	vorbis_info_clear(&vi);
 
-  ogg_sync_clear(&sync_in);
-  ogg_sync_clear(&sync_out);
+	ogg_sync_clear(&sync_in);
+	ogg_sync_clear(&sync_out);
 
-  return code;
+	return code;
 }
